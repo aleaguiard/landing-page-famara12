59d405832ffa5f24d40f0c4f57fe147c5f0c6758
Ale Aguiar Díaz <122371223+aleaguiard@users.noreply.github.com>
2024-08-05 13:11:47 +0100

    feat(carousel): ensure only one image is visible at a time and adjust styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,10 +7,10 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundImage: {
-				hero: "url('./BG1.jpg')",
-				about: "url('/aboutpage-bg.png')",
-				services: "url('/servicespage-bg.jpg')",
-				contact: "url('/contactpage-bg.jpg')",
+				index: "url('./BG1.jpg')",
+				properties: "url('./BG2.jpg')",
+				booking: "url('./BG3.jpg')",
+				contact: "url('./BG5.jpg')",
 			},
 			fontFamily: {
 				dm: ['DM Serif Display', 'serif'],
